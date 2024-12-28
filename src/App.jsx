d4626c699f5e775660dc3329a4b84bdfe4ae9b9d
Hamzah <hamzahchhapra2004@gmail.com>
2024-12-28 15:51:58 +0530

    Navbar header, items and button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1 className="text-6xl font-extrabold">Hello</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+function Navbar() {
+  return (
+    <div className="bg-background py-4 px-10 flex justify-between items-center">
+      <h1 className="text-4xl px-10 py-1 text-text font-extrabold">Tora</h1>
+      <ul className="hidden md:flex py-2 rounded-xl text-text">
+        <a href="/" className="px-3 mx-3 duration-150 hover:font-extrabold">
+          Home
+        </a>
+        <a href="/" className="px-3 mx-3 duration-150 hover:font-extrabold">
+          About
+        </a>
+        <a href="/" className="px-3 mx-3 duration-150 hover:font-extrabold">
+          Service
+        </a>
+        <a href="/" className="px-3 mx-3 duration-150 hover:font-extrabold">
+          Team
+        </a>
+      </ul>
+      <a
+        href="/"
+        className="text-2xl text-text font-extrabold px-8 py-1.5 md:py-2 border-2 border-black rounded-full duration-150 hover:text-white hover:bg-black"
+      >
+        Contact
+      </a>
+    </div>
+  );
+}
+
+export default Navbar;
