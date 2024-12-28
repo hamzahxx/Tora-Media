5baef0c581aa2dfd8b80cb20eea01d1d15a490ca
Hamzah <hamzahchhapra2004@gmail.com>
2024-12-28 15:27:52 +0530

    Tailwind Color Extention Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      background: "#fffbf0",
-      primary: "#fcb827",
-      secondary: "#faf4db",
-      accent: "#ceadff",
-      text: "#0e0c0d",
+    extend: {
+      colors: {
+        background: "#fffbf0",
+        primary: "#fcb827",
+        secondary: "#faf4db",
+        accent: "#ceadff",
+        text: "#0e0c0d",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
