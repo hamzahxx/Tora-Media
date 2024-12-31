fb0d0e4b3f5c540a2ba42ec16bbe9223f9f76851
Hamzah <hamzahchhapra2004@gmail.com>
2024-12-31 17:24:36 +0530

    Hero page
    - Added a new Hero.jsx component
    - Added SVG called Marketing-bro.svg to use in Hero component
    - Added Hero title and CTA buttons
    - Updated App.jsx to include Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+import marketing from "../assets/Marketing-bro.svg";
+
+function Hero() {
+  return (
+    <div className="bg-background flex flex-col-reverse lg:flex-row justify-center items-center h-[100%] lg:h-[88vh] py-4 px-10 pt-10 lg:pt-0">
+      <div className="px-10 pt-3 lg:pt-10">
+        <div>
+          <h1 className="text-5xl lg:text-7xl text-center lg:text-left">
+            Increase Your Leads by{" "}
+            <span className="text-6xl lg:text-8xl font-bold italic font-mono">
+              10x
+            </span>
+          </h1>
+        </div>
+        <div className="flex justify-center lg:justify-start">
+          <a
+            href="/"
+            className="text-center bg-primary hover:bg-green-300 duration-150 rounded-3xl border-4 border-black font-bold my-6 mr-4 px-4 py-2"
+          >
+            Book a Free Consultation
+          </a>
+          <a
+            href="/"
+            className="text-center hover:bg-accent duration-150 rounded-3xl border-4 border-black font-bold my-6 ml-4 px-4 py-2"
+          >
+            Learn More
+          </a>
+        </div>
+      </div>
+      <img
+        src={marketing}
+        className="lg:px-10 lg:py-0 h-[45vh] lg:h-[70vh] w-auto"
+        alt="Marketing Image"
+      />
+    </div>
+  );
+}
+
+export default Hero;
