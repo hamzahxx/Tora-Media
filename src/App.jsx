c6df1fad9ac873fd8ea5dc2acc1126b2d24bae4c
Hamzah <hamzahchhapra2004@gmail.com>
2025-01-10 18:58:05 +0530

    feature/homepage
    - added layered-peaks-haikei.svg as a divider to work on about section next.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import layer from "./assets/layered-peaks-haikei.svg";
 
 function App() {
   return (
     <div className="bg-background absolute z-0 w-screen h-screen">
       <Navbar />
       <Hero />
+      <img
+        src={layer}
+        className="w-[100vw] h-auto absolute top-[75vh] md:top-[40vh] lg:top-[12vh] z-[-1]"
+      />
     </div>
   );
 }
